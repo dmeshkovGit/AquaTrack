051bdcccb6d5186182172ff1759f9474358adb3b
LytvyniukV <lytvyniuk.vladyslav@gmail.com>
2024-07-11 19:02:57 +0200

    userBar

--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -1,10 +1,28 @@
 import css from '../UserBar/UserBar.module.css';
 import UserBarPopover from '../../components/UserBarPopover/UserBarPopover';
-
+import { IoIosArrowDown } from 'react-icons/io';
+import { IoIosArrowUp } from 'react-icons/io';
+import { RxAvatar } from 'react-icons/rx';
+import { Popover } from 'react-tiny-popover';
+import { useState } from 'react';
 export default function UserBar() {
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+
   return (
-    <div>
-      <UserBarPopover />
-    </div>
+    <Popover
+      isOpen={isPopoverOpen}
+      positions={['bottom']}
+      onClickOutside={() => setIsPopoverOpen(!isPopoverOpen)}
+      content={<UserBarPopover />}
+    >
+      <button
+        className={css.btn}
+        onClick={() => setIsPopoverOpen(!isPopoverOpen)}
+      >
+        <p className={css.name}>Username</p>
+        <RxAvatar />
+        {isPopoverOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}
+      </button>
+    </Popover>
   );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,10 @@
+import UserPanel from '../../components/UserPanel/UserPanel';
 import WelcomeSection from '../../components/WelcomeSection/WelcomeSection';
 
 export default function HomePage() {
   return (
     <div>
+      <UserPanel />
       <WelcomeSection />
     </div>
   );
