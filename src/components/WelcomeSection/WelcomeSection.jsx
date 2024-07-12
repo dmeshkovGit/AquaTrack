fc13eebc35e3764ae7c22bc05245eac792dc40f2
Mykhailo Hrunenko <michagrinenko@gmail.com>
2024-07-12 10:02:42 -0800

    icons & photos

--- src/components/WelcomeSection/WelcomeSection.jsx
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -2,7 +2,6 @@
 import { Link } from 'react-router-dom';
 import Logo from '../../shared/components/Logo/Logo';
 import styles from './WelcomeSection.module.css';
-import Modal from '../../shared/components/Modal/Modal';
 
 export default function WelcomeSection() {
   return (
