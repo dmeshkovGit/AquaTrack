6c684f59f02462ec31f8abd1b9f10925d917c095
01eksandr0 <krux228@gmail.com>
2024-07-13 10:57:26 +0300

    use style var

--- src/components/CalendarPagination/CalendarPagination.jsx
+++ src/components/CalendarPagination/CalendarPagination.jsx
@@ -1,16 +1,56 @@
+import { useState, useEffect } from 'react';
 import Icon from '../../shared/components/Icon/Icon';
 import css from '../CalendarPagination/CalendarPagination.module.css';
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 
 export default function CalendarPagination() {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    setDate(new Date());
+  }, []);
+
+  const handlePrevMonth = () => {
+    setDate(prevDate => {
+      const newDate = new Date(prevDate);
+      newDate.setMonth(newDate.getMonth() - 1);
+      return newDate;
+    });
+  };
+
+  const handleNextMonth = () => {
+    setDate(prevDate => {
+      const newDate = new Date(prevDate);
+      newDate.setMonth(newDate.getMonth() + 1);
+      return newDate;
+    });
+  };
+
+  const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+
   return (
     <div className={css.paginationContainer}>
       <div className={css.buttonsContainer}>
-        <button className={css.button}>
+        <button className={css.button} onClick={handlePrevMonth}>
           <FaAngleLeft />
         </button>
-        <p className={css.title}>April, 2024</p>
-        <button className={css.button}>
+        <p className={css.title}>
+          {monthNames[date.getMonth()]}, {date.getFullYear()}
+        </p>
+        <button className={css.button} onClick={handleNextMonth}>
           <FaAngleRight />
         </button>
       </div>
--- src/components/MonthInfo/MonthInfo.jsx
+++ src/components/MonthInfo/MonthInfo.jsx
@@ -1,11 +1,8 @@
 import css from '../MonthInfo/MonthInfo.module.css';
 import CalendarPagination from '../../components/CalendarPagination/CalendarPagination';
 import Calendar from '../../components/Calendar/Calendar';
-import { useState } from 'react';
 
 export default function MonthInfo() {
-  const [activeMount, setActiveMount] = useState();
-  const [activeYear, setActiveYear] = useState();
   return (
     <div className={css.monthInfoContainer}>
       <div className={css.monthInfoPaginationContainer}>
