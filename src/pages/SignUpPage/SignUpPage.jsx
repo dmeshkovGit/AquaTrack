bbc2af07ece3818b0037fc4c7a0940dbf8f16b0f
DAT <terlovoy77@gmail.com>
2024-07-13 16:54:44 +0300

    SignUpPage

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,11 +1,24 @@
 import { Link } from 'react-router-dom';
 import Logo from '../../shared/components/Logo/Logo';
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
+import css from '../SignUpPage/SignUpPage.module.css';
+
 export default function SignUpPage() {
+  const handleSignUp = data => {
+    console.log('Form Data:', data);
+    // логіка для надсилання даних на сервер
+  };
+
   return (
-    <div>
+    <div className={css.container}>
       <Logo />
-      <SignUpForm />
+      <h2>Sign Up</h2>
+      <SignUpForm onSubmit={handleSignUp} />
+      <p>
+        Already have an account? <a href="/signin">Sign In</a>
+        {/* or use Link */}
+        <Link to="/signin">Sign In</Link>
+      </p>
       <Link to="/"> Home</Link>
     </div>
   );
