c23ae316dc99347b4c33be0789c6354e6239c086
IgBovt <140014814+IgBovt@users.noreply.github.com>
2024-07-14 00:16:04 +0300

    made progressbar

--- src/components/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/WaterProgressBar/WaterProgressBar.jsx
@@ -1,5 +1,23 @@
 import css from '../WaterProgressBar/WaterProgressBar.module.css';
+import { useState } from 'react';
 
 export default function WaterProgressBar() {
-  return <div>WaterProgressBar</div>;
+  const [percent, setPercent] = useState('30');
+  return (
+    <div className={css.wrapper}>
+      <h6 className={css.header}> Today</h6>
+      <div className={css.bar}>
+        <div className={css.progressLine} style={{ width: `${percent}%` }}>
+          <span className={css.activePercent} style={{ left: `100%` }}>
+            {`${percent}%`}
+          </span>
+        </div>
+      </div>
+      <div className={css.textContainer}>
+        <p className={css.text}>0%</p>
+        <p className={css.text}>50%</p>
+        <p className={css.text}>100%</p>
+      </div>
+    </div>
+  );
 }
