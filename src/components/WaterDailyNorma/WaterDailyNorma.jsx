a7ae76d2fe0c93ca87bcd8cce4422d801e6d9b84
IgBovt <140014814+IgBovt@users.noreply.github.com>
2024-07-13 17:12:59 +0300

    Add Water main Norma

--- src/components/WaterDailyNorma/WaterDailyNorma.jsx
+++ src/components/WaterDailyNorma/WaterDailyNorma.jsx
@@ -1,5 +1,10 @@
 import css from '../WaterDailyNorma/WaterDailyNorma.module.css';
 
 export default function WaterDailyNorma() {
-  return <div>WaterDailyNorma</div>;
+  return (
+    <div className={css.container}>
+      <p className={css.waterCount}>1.5 L</p>
+      <p className={css.text}>My daily norma</p>
+    </div>
+  );
 }
