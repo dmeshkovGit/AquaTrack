a0dda5d0f2a8f93cf7c93dfe71baa99b6c71c98e
IgBovt <140014814+IgBovt@users.noreply.github.com>
2024-07-13 22:46:17 +0300

    add hovers

--- src/components/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -2,32 +2,13 @@ import css from '../WaterMainInfo/WaterMainInfo.module.css';
 import WaterDailyNorma from '../../components/WaterDailyNorma/WaterDailyNorma';
 import WaterProgressBar from '../../components/WaterProgressBar/WaterProgressBar';
 import AddWaterBtn from '../../shared/components/AddWaterBtn/AddWaterBtn';
-import Modal from '../../shared/components/Modal/Modal';
-import WaterModal from '../../shared/components/WaterModal/WaterModal';
-import { useState } from 'react';
 
 export default function WaterMainInfo() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
-  const handleOpenModal = () => {
-    setIsOpenModal(true);
-  };
-
   return (
     <div className={css.WaterMainInfoContainer}>
       <WaterDailyNorma />
       <WaterProgressBar />
-      <AddWaterBtn handleOpen={handleOpenModal} addStyle={false} />
-      {isOpenModal && (
-        <Modal
-          isOpen={isOpenModal}
-          onClose={() => {
-            setIsOpenModal(false);
-          }}
-        >
-          <WaterModal />
-        </Modal>
-      )}
+      <AddWaterBtn addStyle={false} />
     </div>
   );
 }
--- src/shared/components/AddWaterBtn/AddWaterBtn.jsx
+++ src/shared/components/AddWaterBtn/AddWaterBtn.jsx
@@ -1,23 +1,45 @@
 import Icon from '../Icon/Icon';
 import clsx from 'clsx';
 import css from '../AddWaterBtn/AddWaterBtn.module.css';
+import WaterModal from '../../components/WaterModal/WaterModal';
+import Modal from '../Modal/Modal';
 
-export default function AddWaterBtn({ handleOpen, addStyle }) {
+import { useState } from 'react';
+
+export default function AddWaterBtn({ addStyle }) {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsOpenModal(true);
+  };
+
   return (
-    <button
-      className={clsx(css.btn, addStyle && css.addStyle)}
-      type="button"
-      onClick={() => {
-        handleOpen();
-      }}
-    >
-      <Icon
-        className={clsx(css.icon, addStyle && css.addIcon)}
-        width="16"
-        height="16"
-        id="upload"
-      />
-      Add water
-    </button>
+    <>
+      <button
+        className={clsx(css.btn, addStyle && css.addBtnStyle)}
+        type="button"
+        onClick={() => {
+          handleOpenModal();
+        }}
+      >
+        <Icon
+          className={clsx(css.icon, addStyle && css.addIconStyle)}
+          width="16"
+          height="16"
+          id="upload"
+        />
+        Add water
+      </button>
+      {isOpenModal && (
+        <Modal
+          isOpen={isOpenModal}
+          onClose={() => {
+            setIsOpenModal(false);
+          }}
+        >
+          <WaterModal />
+        </Modal>
+      )}
+    </>
   );
 }
