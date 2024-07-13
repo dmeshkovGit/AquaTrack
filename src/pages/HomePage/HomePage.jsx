af520c5ef24e8b87a958fb3b485f1d38ebb1f1d5
Dmitriy Mieshkov <dmitriymeshkov.a@gmail.com>
2024-07-14 01:13:16 +0300

    fix

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,9 @@
-import MonthInfo from '../../components/MonthInfo/MonthInfo';
 import WelcomeSection from '../../components/WelcomeSection/WelcomeSection';
 
 export default function HomePage() {
   return (
     <div>
       <WelcomeSection />
-      <MonthInfo />
     </div>
   );
 }
