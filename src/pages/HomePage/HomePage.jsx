a6138cbb85020450d74d36d7eec79d85daae1dab
neromaxor <141302051+neromaxor@users.noreply.github.com>
2024-07-14 03:05:57 +0300

    update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import MonthInfo from '../../components/MonthInfo/MonthInfo';
 import WelcomeSection from '../../components/WelcomeSection/WelcomeSection';
+import AdvantagesSection from '../../components/AdvantagesSection/AdvantagesSection';
 
 export default function HomePage() {
   return (
     <div>
       <WelcomeSection />
+      <AdvantagesSection />
       <MonthInfo />
     </div>
   );
