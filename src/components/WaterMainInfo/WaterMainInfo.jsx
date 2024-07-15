df19475e9a30601279a1a3cbe9fb423061c5e32e
Dmitriy Mieshkov <dmitriymeshkov.a@gmail.com>
2024-07-15 03:57:24 +0300

    add logo

--- src/components/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -2,10 +2,14 @@ import css from '../WaterMainInfo/WaterMainInfo.module.css';
 import WaterDailyNorma from '../../components/WaterDailyNorma/WaterDailyNorma';
 import WaterProgressBar from '../../components/WaterProgressBar/WaterProgressBar';
 import AddWaterBtn from '../../shared/components/AddWaterBtn/AddWaterBtn';
+import Logo from '../../shared/components/Logo/Logo';
 
 export default function WaterMainInfo() {
   return (
     <div className={css.WaterMainInfoContainer}>
+      <div className={css.logoWraper}>
+        <Logo />
+      </div>
       <WaterDailyNorma />
       <WaterProgressBar />
       <AddWaterBtn />
