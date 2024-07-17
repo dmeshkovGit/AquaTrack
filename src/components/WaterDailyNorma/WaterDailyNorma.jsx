76a430cccdac24ed5a84a760a4eebc33cdde65c3
IgBovt <140014814+IgBovt@users.noreply.github.com>
2024-07-18 01:49:39 +0300

    done

--- src/components/WaterDailyNorma/WaterDailyNorma.jsx
+++ src/components/WaterDailyNorma/WaterDailyNorma.jsx
@@ -1,9 +1,13 @@
+import { useSelector } from 'react-redux';
 import css from '../WaterDailyNorma/WaterDailyNorma.module.css';
+import { selectUserWaterNorm } from '../../redux/user/selectors';
 
 export default function WaterDailyNorma() {
+  const countWater = useSelector(selectUserWaterNorm);
+
   return (
     <div className={css.container}>
-      <p className={css.waterCount}>1.5 L</p>
+      <p className={css.waterCount}>{countWater} L</p>
       <p className={css.text}>My daily norma</p>
     </div>
   );
