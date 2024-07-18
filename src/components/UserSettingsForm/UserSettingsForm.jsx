96604a72c0b0073f3a6b41931558926586f3441d
Dmitriy Mieshkov <dmitriymeshkov.a@gmail.com>
2024-07-18 23:04:27 +0300

    fix

--- src/components/UserSettingsForm/UserSettingsForm.jsx
+++ src/components/UserSettingsForm/UserSettingsForm.jsx
@@ -9,9 +9,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { selectIsLoading, selectUser } from '../../redux/user/selectors';
 import { updateUser } from '../../redux/user/operations';
 import toast from 'react-hot-toast';
-import { Loader } from '../Loader/Loader';
+import { Loader } from '../../shared/components/Loader/Loader';
 import { isNumber } from '../../helpers/validationsHelper';
-
 
 const schema = yup.object().shape({
   gender: yup.string().required('Option is required'),
