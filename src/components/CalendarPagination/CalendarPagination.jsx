1b5ddbb6ad750626d773c8058040ebd35199e4bc
neromaxor <141302051+neromaxor@users.noreply.github.com>
2024-07-20 22:14:50 +0300

    update

--- src/components/CalendarPagination/CalendarPagination.jsx
+++ src/components/CalendarPagination/CalendarPagination.jsx
@@ -1,14 +1,44 @@
+import { useState } from 'react';
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 import css from './CalendarPagination.module.css';
-import Icon from '../../shared/components/Icon/Icon';
+import { useTranslation } from 'react-i18next';
+import '../../translate/index.js';
 
-export default function CalendarPagination({
-  handlePrevMonth,
-  monthNames,
-  handleNextMonth,
-  date,
-  isOpen,
-}) {
+export default function CalendarPagination({ isOpen }) {
+  const [date, setDate] = useState(new Date());
+  const { t } = useTranslation();
+
+  const handlePrevMonth = () => {
+    setDate(prevDate => {
+      const newDate = new Date(prevDate);
+      newDate.setMonth(newDate.getMonth() - 1);
+      return newDate;
+    });
+  };
+
+  const handleNextMonth = () => {
+    setDate(prevDate => {
+      const newDate = new Date(prevDate);
+      newDate.setMonth(newDate.getMonth() + 1);
+      return newDate;
+    });
+  };
+
+  const monthNames = [
+    t('Month january'),
+    t('Month february'),
+    t('Month march'),
+    t('Month april'),
+    t('Month may'),
+    t('Month june'),
+    t('Month july'),
+    t('Month august'),
+    t('Month september'),
+    t('Month october'),
+    t('Month november'),
+    t('Month december'),
+  ];
+
   return (
     <div className={css.paginationContainer}>
       <div className={css.buttonsContainer}>
@@ -22,13 +52,23 @@ export default function CalendarPagination({
           <FaAngleRight />
         </button>
       </div>
-      <Icon
-        onClick={isOpen}
-        className={css.icon}
-        id="pieChart"
-        height={20}
-        width={20}
-      />
+      <div className={css.iconContainer}>
+        <div
+          className={css.icon}
+          id="pieChart"
+          style={{
+            cursor: 'pointer',
+            width: 24,
+            height: 24,
+            backgroundColor: 'red',
+            borderRadius: '50%',
+          }}
+          onClick={() => {
+            console.log('Div clicked');
+            isOpen();
+          }}
+        ></div>
+      </div>
     </div>
   );
 }
--- src/components/MonthInfo/MonthInfo.jsx
+++ src/components/MonthInfo/MonthInfo.jsx
@@ -1,81 +1,28 @@
+import { useState } from 'react';
 import CalendarPagination from '../../components/CalendarPagination/CalendarPagination';
 import Calendar from '../../components/Calendar/Calendar';
+import WaterConsumption from '../../components/WaterConsumption/origin/WaterConsumption.jsx';
 import css from './MonthInfo.module.css';
-import { useEffect, useState } from 'react';
-
+import { useTranslation } from 'react-i18next';
 import '../../translate/index.js';
-import { getMonthInfo } from '../../API/apiOperations.js';
 
 export default function MonthInfo() {
-  const [date, setDate] = useState(new Date());
   const [showChart, setShowChart] = useState(false);
-  const [daysList, setDays] = useState([]);
+  const { t } = useTranslation();
 
   const toggleView = () => {
-    console.log('Icon clicked');
     setShowChart(prevShowChart => !prevShowChart);
   };
 
-  useEffect(() => {
-    setDate(new Date());
-    getMonthInfo();
-  }, []);
-
-  useEffect(() => {
-    const getMonth = async () => {
-      try {
-        const response = await getMonthInfo(new Date(date).getTime());
-        if (response) setDays(response);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getMonth();
-  }, [date]);
-
-  const handlePrevMonth = () => {
-    setDate(prevDate => {
-      const newDate = new Date(prevDate);
-      newDate.setMonth(newDate.getMonth() - 1);
-      return newDate;
-    });
-  };
-
-  const handleNextMonth = () => {
-    setDate(prevDate => {
-      const newDate = new Date(prevDate);
-      newDate.setMonth(newDate.getMonth() + 1);
-      return newDate;
-    });
-  };
-
-  const monthNames = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
   return (
     <div className={css.monthInfoContainer}>
       <div className={css.monthInfoPaginationContainer}>
-        <h2 className={css.title}>Month</h2>
-        <CalendarPagination
-          handlePrevMonth={handlePrevMonth}
-          handleNextMonth={handleNextMonth}
-          monthNames={monthNames}
-          date={date}
-          isOpen={toggleView}
-        />
+        <h2 className={css.title}>
+          {showChart ? 'Statistics' : t('Month water')}
+        </h2>
+        <CalendarPagination isOpen={toggleView} />
       </div>
-      <Calendar daysList={daysList} />
+      {showChart ? <WaterConsumption /> : <Calendar />}
     </div>
   );
 }
